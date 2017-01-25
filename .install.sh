563ef687c98c312b09ebc863627ac94f53244ea2
volkovasystems <richeve.bebedor@gmail.com>
2017-01-25 12:50:33 +0800

    Updated dependency.

---
+++ .install.sh
@@ -0,0 +1,31 @@
+yarn add \
+babel-runtime \
+arid \
+budge \
+called \
+depher \
+filled \
+harden \
+protype \
+truly \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+arid \
+budge \
+called \
+depher \
+filled \
+harden \
+protype \
+truly \
+zelf \
+--save
